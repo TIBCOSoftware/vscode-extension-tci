12ff56e7e7e8967440ac3f7fce61ff32c6a630df
retgits <lstigter+github@gmail.com>
2017-04-04 12:59:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# tci-tools
+
+![release 0.0.2](https://img.shields.io/badge/release-0.0.2-brightgreen.png)
+
+Out of the box support for TIBCO(R) Cloud Integration right in VSCode
+
+## Features
+
+### Check installation status
+This command checks whether or not the `tibcli` utility has been installed on the machine at the location you have configured (see below for details on `extension settings`).
+
+### Other available commands
+![Image](images/commandlist.jpg)
+
+* __Check installation status__: Check whether the `tibcli` utility exists in the location you have specified
+* __Open API Modeler__: Launch the TIBCO Cloud Integration - API Modeler in a browser
+* __Generate deployment artifacts__: Create the manifest and zipfile to be deployed to TIBCO Cloud Integration
+* __Push Node.js app to TCI__: Not only create the manifest and zipfile, but push it to TIBCO Cloud Integration as well
+* __Launch Node.js app locally (runs npm install and node . in a terminal)__: Install dependencies (from package.json) and run your Node.js app locally to test 
+* __Open TIBCO Community__: Launch the TIBCO Community in a browser
+* __Open TIBCO Cloud Integration documentation__: Launch the TIBCO Cloud Integration documentation in a browser
+
+## Requirements
+
+In order to use this plugin you would need a valid account to TIBCO Cloud Integration (you can sign up at https://cloud.tibco.com/free-trial) and the `tibcli` utility (see https://integration.cloud.tibco.com/docs/getstarted/installation/download-tools.html for details)
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `tci.tibcli`: The full qualified path to the tibcli executable (including .exe on Windows) which is set to `c:/tmp/tibcli.exe` by default
+
+## Known Issues
+
+This release has been tested on Microsoft Windows 10 (with PowerShell) and macOS 10.12 (with Bash)
+
+## Questions?
+Feel free to open a Github issue or ask a question on the [TIBCO Community](https://community.tibco.com)
+
+## Contributors
+[Leon Stigter](https://github.com/retgits)
+
+## Release Notes
+
+### 0.0.2
+Added support for macOS 10.12 (Sierra)
+
+### 0.0.1
+Initial release of tci-tools on Windows
---
+++ extension.js
@@ -0,0 +1,193 @@
+var vscode = require('vscode');
+var fs = require('fs');
+
+var foundTibcli = false;
+
+const tciDocumentationURL = 'https://integration.cloud.tibco.com/docs/index.html';
+const tciCommunityURL = 'https://community.tibco.com/products/tibco-cloud-integration';
+const tciAPIModelerURL = 'https://integration.cloud.tibco.com/apispecs';
+const tciToolsDocumentationURL = 'https://github.com/TIBCOSoftware/tci-samples/'
+
+function activate(context) {
+    var terminal = vscode.window.createTerminal('tci-tools');
+    var statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+
+    registerCommands();
+
+    function registerCommands() {
+        var disposable = vscode.commands.registerCommand('tci.checkInstallationStatus', function () {
+            checkInstallationStatus(true);
+        });
+
+        var disposable = vscode.commands.registerCommand('tci.generateDeploymentArtifacts', function () {
+            generateDeploymentArtifacts();
+        });
+
+        var disposable = vscode.commands.registerCommand('tci.pushNodejsApp', function () {
+            generateDeploymentArtifacts(true);
+        });
+
+        var disposable = vscode.commands.registerCommand('tci.launchAPIModeler', function () {
+            launchBrowserWithURL(tciAPIModelerURL);
+        });
+
+        var disposable = vscode.commands.registerCommand('tci.launchNodeAppLocally', function () {
+            launchNodeAppLocally();
+        });
+
+        var disposable = vscode.commands.registerCommand('tci.launchTIBCOCommunity', function () {
+            launchBrowserWithURL(tciCommunityURL);
+        });
+
+        var disposable = vscode.commands.registerCommand('tci.launchTCIDocs', function () {
+            launchBrowserWithURL(tciDocumentationURL);
+        });
+
+        context.subscriptions.push(disposable);
+    }
+
+    function launchBrowserWithURL(url) {
+        vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(url));
+    }
+
+    function launchNodeAppLocally() {
+        var workspaceFolder = vscode.workspace.rootPath;
+
+        if (workspaceFolder == null) {
+            vscode.window.showInformationMessage('To start a Node.js app locally you need to select a folder in Visual Studio Code (this is the folder that contains your server.js file)');
+            return;
+        }
+
+        terminal.show(true);
+        terminal.sendText('npm install', true);
+        terminal.sendText('node .', true);
+    }
+
+    function checkInstallationStatus(showSuccessMessage) {
+        statusBarItem.text = "Checking if tibcli exists...";
+        statusBarItem.show();
+
+        var tibcli = vscode.workspace.getConfiguration('tci').get('tibcli');
+
+        if (tibcli == null) {
+            var messageItems = ['The tibcli location is not set, visit ' + tciToolsDocumentationURL + ' for instructions']
+            messageItems.push('Open in browser');
+            vscode.window.showInformationMessage.apply(this, messageItems).then(function (value) {
+                if (value == 'Open in browser') {
+                    launchBrowserWithURL(tciToolsDocumentationURL);
+                } else {
+                    disposeStatusbar();
+                }
+            });
+            setTibcliFound(false);
+            return false;
+        }
+
+        var tibcliExists = fs.existsSync(tibcli);
+
+        if (!tibcliExists) {
+            var messageItems = ['tibcli doesn\'t exist or isn\'t accessible, visit ' + tciToolsDocumentationURL + ' for instructions']
+            messageItems.push('Open in browser');
+            vscode.window.showInformationMessage.apply(this, messageItems).then(function (value) {
+                if (value == 'Open in browser') {
+                    launchBrowserWithURL(tciToolsDocumentationURL);
+                } else {
+                    disposeStatusbar();
+                }
+            });
+            setTibcliFound(false);
+            return false;
+        } else {
+            setTibcliFound(true);
+            var messageItems = ['tibcli was found on your system. You\'re ready to go!']
+            if (showSuccessMessage) {
+                vscode.window.showInformationMessage.apply(this, messageItems).then(function () {
+                    disposeStatusbar();
+                });
+            }
+            return true;
+        }
+    }
+
+    function generateDeploymentArtifacts(pushOnSuccess) {
+        if (!isTibcliFound()) {
+            var isTibcliNowFound = checkInstallationStatus();
+            if (!isTibcliNowFound) {
+                return;
+            }
+        }
+
+        var workspaceFolder = vscode.workspace.rootPath;
+
+        if (workspaceFolder == null) {
+            vscode.window.showInformationMessage('To push an app to TCI you need to select a folder in Visual Studio Code');
+            return;
+        }
+
+        if (/^win/.test(process.platform)) {
+
+            var parentFolder = workspaceFolder.substring(0, workspaceFolder.lastIndexOf('\\'));
+            var tibcli = vscode.workspace.getConfiguration('tci').get('tibcli');
+
+            terminal.show(true);
+            terminal.sendText('New-Item -Path ' + parentFolder + ' -Name "deployment" -ItemType "directory" | Out-Null', true);
+            terminal.sendText('Copy-Item ../manifest.json ../deployment | Out-Null', true);
+            terminal.sendText('Get-ChildItem . | where { $_.Name -notin "node_modules"} | Compress-Archive -DestinationPath ../deployment/app.zip -Force | Out-Null', true);
+            terminal.sendText('cd ../deployment', true);
+
+            if (pushOnSuccess) {
+                terminal.sendText(tibcli + ' app push', true);
+            } else {
+                var messageItems = ['Your deployment artifacts have been built... open file location?']
+                messageItems.push('Yes');
+                vscode.window.showInformationMessage.apply(this, messageItems).then(function (value) {
+                    if (value == 'Yes') {
+                        terminal.sendText('Invoke-Item ' + parentFolder + '\\deployment', true);
+                    }
+                });
+            }
+        } if (/^darwin/.test(process.platform)) {
+
+            var parentFolder = workspaceFolder.substring(0, workspaceFolder.lastIndexOf('/'));
+            var tibcli = vscode.workspace.getConfiguration('tci').get('tibcli');
+
+            terminal.show(true);
+            terminal.sendText('mkdir -p ' + parentFolder + '/deployment', true);
+            terminal.sendText('cp ../manifest.json ../deployment', true);
+            terminal.sendText('zip -r -X ../deployment/app.zip . -x "node_modules"', true);
+            terminal.sendText('cd ../deployment', true);
+
+            if (pushOnSuccess) {
+                terminal.sendText(tibcli + ' app push', true);
+            } else {
+                var messageItems = ['Your deployment artifacts have been built... open file location?']
+                messageItems.push('Yes');
+                vscode.window.showInformationMessage.apply(this, messageItems).then(function (value) {
+                    if (value == 'Yes') {
+                        //terminal.sendText('Invoke-Item ' + parentFolder + '\\deployment',true);
+                        terminal.sendText('open .', true);
+                    }
+                });
+            }
+        } else {
+            vscode.window.showErrorMessage('This command is only supported on ' + process.platform);
+        }
+    }
+
+    function disposeStatusbar() {
+        statusBarItem.hide();
+    }
+
+    function setTibcliFound(found) {
+        foundTibcli = found;
+    }
+
+    function isTibcliFound() {
+        return foundTibcli;
+    }
+}
+exports.activate = activate;
+
+function deactivate() {
+}
+exports.deactivate = deactivate;
