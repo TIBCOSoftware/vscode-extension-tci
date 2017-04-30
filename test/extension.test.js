e7942275535781032976dd6cc868df23cf491767
retgits <lstigter+github@gmail.com>
2017-04-30 12:50:01 -0700

    Added license text

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,3 +1,9 @@
+/*
+Copyright © 2017. TIBCO Software Inc.
+This file is subject to the license terms contained
+in the license file that is distributed with this file.
+*/
+
 /* global suite, test */
 
 //
